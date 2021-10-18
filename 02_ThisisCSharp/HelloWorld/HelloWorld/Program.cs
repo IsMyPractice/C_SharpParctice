9e3d949556c67246c8b47503fbe94387d339ea07
JHLee <kerneltrek@gmail.com>
2021-10-18 15:11:36 +0900

    2-1) hello World 인수 넣기 테스트

---
+++ 02_ThisisCSharp/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("사용법 : HelloWorld.exe <이름>");
+                return;
+            }
+            Console.WriteLine($"Hello, {args[0]}!");
+        }
+    }
+}
